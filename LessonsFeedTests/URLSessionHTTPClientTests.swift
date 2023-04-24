12b8dfc4fd934bf7d15069d60ff97a6cba61a40a
Nav <navdeep.rana@heyhub.com>
2023-04-24 13:29:27 +0530

    Make stub function more explicit to reflect all combinations of server response

--- LessonsFeedTests/URLSessionHTTPClientTests.swift
+++ LessonsFeedTests/URLSessionHTTPClientTests.swift
@@ -30,7 +30,29 @@ final class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStubs.registerStub()
         let url = URL(string: "https://iphonephotographyschool.com/test-api/lessons")!
         let error = anyError()
-        URLProtocolStubs.stub(url: url, error: error)
+        URLProtocolStubs.stub(url: url, data: nil, response: nil, error: error)
+        let sut = URLSessionHTTPClient()
+        let exp = expectation(description: "Wait for request to complete")
+        sut.get(from: url) { result in
+            switch result{
+            case let .failure(receivedError as NSError):
+                XCTAssertEqual(receivedError.domain, error.domain)
+            case .success(_,_):
+                XCTFail("Expected failure but got success instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        URLProtocolStubs.unRegisterStub()
+    }
+    
+    func test_getFromURL_getsData(){
+        URLProtocolStubs.registerStub()
+        let url = URL(string: "https://iphonephotographyschool.com/test-api/lessons")!
+        let error = anyError()
+        URLProtocolStubs.stub(url: url, data: nil, response: nil, error: error)
         let sut = URLSessionHTTPClient()
         let exp = expectation(description: "Wait for request to complete")
         sut.get(from: url) { result in
@@ -55,11 +77,13 @@ final class URLSessionHTTPClientTests: XCTestCase {
     private class URLProtocolStubs: URLProtocol{
         private static var stubs = [URL: Stub]()
         private struct Stub{
+            let data: Data?
+            let response: URLResponse?
             let error: Error?
         }
         
-        static func stub(url: URL, error: Error? = nil){
-            URLProtocolStubs.stubs[url] = Stub(error: error)
+        static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?){
+            URLProtocolStubs.stubs[url] = Stub(data: data, response: response, error: error)
         }
         
         static func registerStub(){
@@ -87,6 +111,13 @@ final class URLSessionHTTPClientTests: XCTestCase {
             guard let url = request.url, let stub = URLProtocolStubs.stubs[url] else { return }
             if let error = stub.error{
                 client?.urlProtocol(self, didFailWithError: error)
+            }
+            if let data = stub.data{
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = stub.response{
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
             }
             client?.urlProtocolDidFinishLoading(self)
         }
